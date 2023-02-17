75761a2094f19de4fd5c5c0afa1a331ec88d714b
Okumu16 <2200801100@std.kyu.ac.ug>
2023-02-17 01:35:04 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,9 +10,9 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
-printf("size of an int: %lu byte(s)\n",(unsigned long)sizeof(b))
-printf("size of a long int: %lu byte(s)\n",(unsigned long)sizeof(c));
-printf("size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(d));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b))
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 return (0);
 }
