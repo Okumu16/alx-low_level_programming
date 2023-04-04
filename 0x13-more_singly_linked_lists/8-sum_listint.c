7ea57bd8edeeead97d5a834c9f2d8613b490ec4a
Okumu16 <2200801100@std.kyu.ac.ug>
2023-04-05 00:38:23 +0300

    update

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,6 +1,7 @@
-um_listint - sum of all the data (n) of a listint_t linked list.
+#include "lists.h"
+/**
+ * sum_listint - sum of all the data (n) of a listint_t linked list.
  * @head: pointer to the first node
- *
  * Return: sum all the data
  */
 int sum_listint(listint_t *head)
