15360be836ba8dc9fc3be4de5e50a011579cb6c3
Okumu16 <2200801100@std.kyu.ac.ug>
2023-04-09 14:15:24 +0300

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -13,19 +11,21 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+int fd = open(filename, O_RDONLY);
+char *buffer = (char *) malloc(sizeof(char) * (letters + 1));
+ssize_t bytes_read = read(fd, buffer, letters);
+ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+
 if (filename == NULL)
 return (0);
-
-int fd = open(filename, O_RDONLY);
 
 if (fd == -1)
 return (0);
 
-char *buffer = (char *) malloc(sizeof(char) * (letters + 1));
 if (buffer == NULL)
 return (0);
 
-ssize_t bytes_read = read(fd, buffer, letters);
 if (bytes_read == -1)
 {
 free(buffer);
@@ -34,7 +34,8 @@ return (0);
 }
 
 buffer[bytes_read] = '\0';
-ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+
 if (bytes_written == -1 || bytes_written != bytes_read)
 {
 free(buffer);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
 
 
