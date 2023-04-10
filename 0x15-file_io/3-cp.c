ddc73e83ac05eef95464ef216f4a4736e8499fae
OKUMU REAGAN OTIM <122821617+Okumu16@users.noreply.github.com>
2023-04-10 23:06:00 +0300

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,7 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void check_num_args(int argc)
+/**
+ * check97 - checks for the correct number of arguments
+ * @argc: number of arguments
+ *
+ * Return: void
+ */
+void check97(int argc)
 {
 	if (argc != 3)
 	{
@@ -11,65 +17,98 @@ void check_num_args(int argc)
 	}
 }
 
-void check_file_read(ssize_t check, char *file_name, int fd_source, int fd_destination)
+/**
+ * check98 - checks that file_from exists and can be read
+ * @check: checks if true of false
+ * @file: file_from name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
+void check98(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name);
-		if (fd_source != -1)
-			close(fd_source);
-		if (fd_destination != -1)
-			close(fd_destination);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
 		exit(98);
 	}
 }
 
-void check_file_write(ssize_t check, char *file_name, int fd_source, int fd_destination)
+/**
+ * check99 - checks that file_to was created and/or can be written to
+ * @check: checks if true of false
+ * @file: file_to name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
+void check99(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_name);
-		if (fd_source != -1)
-			close(fd_source);
-		if (fd_destination != -1)
-			close(fd_destination);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
 		exit(99);
 	}
 }
 
-void check_fd_close(int check, int fd)
+/**
+ * check100 - checks that file descriptors were closed properly
+ * @check: checks if true or false
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+void check100(int check, int fd)
 {
 	if (check == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
-
+/**
+ * main - opies the content of a file to another file.
+ * @argc: number of arguments passed
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
-	int source_file_descriptor, destination_file_descriptor, close_destination_fd, close_source_fd;
-	ssize_t bytes_read, bytes_written;
+	int fd_from, fd_to, close_to, close_from;
+	ssize_t lenr, lenw;
 	char buffer[1024];
-	mode_t file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	mode_t file_perm;
 
-	check_num_args(argc);
-
-	source_file_descriptor = open(argv[1], O_RDONLY);
-	check_file_read((ssize_t)source_file_descriptor, argv[1], -1, -1);
-
-	destination_file_descriptor = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
-	check_file_write((ssize_t)destination_file_descriptor, argv[2], source_file_descriptor, -1);
-
-	bytes_read = 1024;
-
-	while (bytes_read == 1024)
+	check97(argc);
+	fd_from = open(argv[1], O_RDONLY);
+	check98((ssize_t)fd_from, argv[1], -1, -1);
+	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	check99((ssize_t)fd_to, argv[2], fd_from, -1);
+	lenr = 1024;
+	while (lenr == 1024)
 	{
-		bytes_read = read(source_file_descriptor, buffer, 1024);
-		check_file_read(bytes_read, argv[1], source_file_descriptor, destination_file_descriptor);
-
-		bytes_written = write(destination_file_descriptor, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-			bytes_written = -1;
-
-		check_file_write(bytes_written, argv[2],
+		lenr = read(fd_from, buffer, 1024);
+		check98(lenr, argv[1], fd_from, fd_to);
+		lenw = write(fd_to, buffer, lenr);
+		if (lenw != lenr)
+			lenw = -1;
+		check99(lenw, argv[2], fd_from, fd_to);
+	}
+	close_to = close(fd_to);
+	close_from = close(fd_from);
+	check100(close_to, fd_to);
+	check100(close_from, fd_from);
+	return (0);
+}
