772463b93f6c668bfaaf2f1f8cd490e71ef0d567
Okumu16 <2200801100@std.kyu.ac.ug>
2023-08-03 14:03:48 +0300

    first commit

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * get_endianness - a function that checks the endianness
+ * Return: 0 if big endian or 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) & i;
+	return (*c == 1);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+Bitwise manipulation in c programming language
